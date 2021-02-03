8e30f9180beb14947e23ef8a0a6fc717fcdea978
Edwin Bartunek <edwin@devhart.io>
2021-02-03 15:37:53 +0900

    PWA support init and seo inputs in studio

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const withPWA = require('next-pwa')
 
-  module.exports = {  
+  module.exports = withPWA({  
+    pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      register: true,
+      sw: 'ms-service-worker.js',
+      publicExcludes: ['!/studio']
+    },
     images: {
       domains: ['cdn.sanity.io','assets.vercel.com'],
     }
-   }
+   })
   
