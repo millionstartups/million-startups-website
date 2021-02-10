56e53040c71d21763c8154a85df771bfc269ddd9
Edwin Bartunek <edwin@devhart.io>
2021-02-10 17:46:28 +0900

    update meta tags

--- components/layout/MainContainer.js
+++ components/layout/MainContainer.js
@@ -227,7 +227,9 @@ const overlay = {
           /> 
           </>
         )}
-
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
       <meta name='apple-mobile-web-app-capable' content='yes' />
       <meta name='apple-mobile-web-app-status-bar-style' content='default' />
       <meta name='apple-mobile-web-app-title' content='The Million Startups' />
